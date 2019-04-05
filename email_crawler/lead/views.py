66c4af8e4fd551546a74568683e6da379b61bd95
Vinicius Ferreira <vfbdelima@gmail.com>
2019-04-05 13:36:13 -0300

    Add route to search for lead emails received
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- email_crawler/lead/views.py
+++ email_crawler/lead/views.py
@@ -1,9 +1,56 @@
-from rest_framework import viewsets
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import LeadSerializer
 from .models import Lead
+from .utils import SCOPES
 
 
 class LeadViewSet(viewsets.ModelViewSet):
 
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+
+    @action(detail=False, methods=['get'])
+    def search_leads(self, request):
+
+        credentials = None
+        module_dir = os.path.dirname(__file__)
+        file_path = os.path.join(module_dir, 'token.pickle')
+
+        try:
+            with open(file_path, 'rb') as token:
+                credentials = pickle.load(token)
+
+        except FileNotFoundError:
+            return Response(
+                {'detail': 'credentials not found in our server'},
+                status.HTTP_404_NOT_FOUND
+            )
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and \
+               credentials.refresh_token:
+                credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                credentials = flow.run_local_server()
+
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(credentials, token)
+
+        service = build('gmail', 'v1', credentials=credentials)
+
+        results = service.users().messages().list(userId='me', q='from:arturbersan@gmail.com').execute()
+
+        return Response(
+            {'detail': 'found {} leads'.format(len(results.get('messages', [])))},
+            status.HTTP_200_OK
+        )
